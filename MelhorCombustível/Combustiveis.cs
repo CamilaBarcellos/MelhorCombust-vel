d522ececc6682f811fe75ed434420dca51c18a11
camila <camilabarcellos.car@gmail.com>
2017-07-17 10:08:44 -0300

    Inclusão de projeto MelhorCombustível

---
+++ MelhorCombustível/Combustiveis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelhorCombustível
+{
+    class Combustiveis
+    {
+        public double ValorLitroAlcool { get; set; }
+        public double ValorLitroGasolina { get; set; }        
+ //       public double ConsumoMedioAlcool { get; set; }
+ //       public double ConsumoMedioGasolina { get; set; }
+        public double ValorLitroCalculado { get; set; }
+        
+    
+        public void CalcularVantagem()
+        {
+            ValorLitroCalculado = (ValorLitroGasolina * 0.7);
+        }
+        
+    }
+}
--- MelhorCombustível/Program.cs
+++ MelhorCombustível/Program.cs
@@ -10,6 +10,34 @@ namespace MelhorCombustível
     {
         static void Main(string[] args)
         {
+            Combustiveis combustiveis = new Combustiveis();
+
+            Console.WriteLine("Digite o valor do litro do Álcool:\n");
+            combustiveis.ValorLitroAlcool = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o valor do litro da Gasolina:\n");
+            combustiveis.ValorLitroGasolina = Convert.ToDouble(Console.ReadLine());
+
+       /*     Console.WriteLine("\nDigite o consumo médio (Km/litro) do carro com Álcool:\n");
+            combustiveis.ConsumoMedioAlcool = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o consumo médio (Km/litro) do carro com Gasolina:\n");
+            combustiveis.ConsumoMedioGasolina = Convert.ToDouble(Console.ReadLine()); */
+
+            Console.WriteLine("\n----------------------------------------------------------------------------");
+
+
+            if(combustiveis.ValorLitroCalculado >= combustiveis.ValorLitroAlcool)
+            {
+                Console.WriteLine("\nÉ mais vantajoso abastecer com álcool, pois o valor é inferior a 70% do valor da gasolina.");
+            }
+            else
+            {
+                Console.WriteLine("\nÉ mais vantajoso abastecer com gasolina, pois o valor do álcool é superior a 70% do seu valor.");
+            }
+            
+            
+            Console.ReadLine();
         }
     }
 }
